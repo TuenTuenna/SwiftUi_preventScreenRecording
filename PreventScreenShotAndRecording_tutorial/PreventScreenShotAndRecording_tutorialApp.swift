f97b0abbf1e3599fab2f6d29690de2115add47d7
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2021-08-26 23:32:19 +0900

    영상 녹화 방지 및 스크린샷 감지 얼럿 띄우기

---
+++ PreventScreenShotAndRecording_tutorial/PreventScreenShotAndRecording_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreventScreenShotAndRecording_tutorialApp.swift
+//  PreventScreenShotAndRecording_tutorial
+//
+//  Created by Jeff Jeong on 2021/08/26.
+//
+
+import SwiftUI
+
+@main
+struct PreventScreenShotAndRecording_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
